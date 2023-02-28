d332e608319e24c013f68a3bd49d407f4aec0ac3
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-28 10:15:19 +0100

    замена элементов массиваположит эл-ты замените на соответст отриц, и наоборот

---
+++ Seminar005_2/Program.cs
@@ -0,0 +1,22 @@
+﻿/* **Задача 32:**Напишите программу замена элементов массива: 
+положительные элементы замените на соответствующие отрицательные, и наоборот.*/
+Console.Clear();
+int[] startArray = GetArray(5,  -10, 10);
+Console.WriteLine(String.Join (", ", startArray));
+Console.WriteLine(String.Join(", ", InverseArray(startArray)));
+
+int [] GetArray (int size, int minValue, int maxValue)
+{
+    int[]array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+    array[i] = new Random().Next(minValue,maxValue+1);
+    }
+    return array;
+}
+int [] InverseArray (int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    array[i]= array[i] * -1;
+    return array;
+}
