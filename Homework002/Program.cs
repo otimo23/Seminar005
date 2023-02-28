5ac5faf68e122473327736499e128f9e5176abd4
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-28 10:23:40 +0100

    Задайте одномерный массив, заполненный случайными числами.Найдите сумму элементов, стоящих на нечётных позициях

---
+++ Homework002/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+Console.Clear();
+int[] array = RanMas(6, 0, 10);
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine($"{SumEvPos(array)}");
+
+int [] RanMas (int size, int minValue, int MaxValue)
+{
+    int[] res = new int [size];
+    for(int i = 0; i < size; i++)
+    {
+    res[i] = new Random().Next(minValue, MaxValue+1);
+    }
+    return res;
+}
+/*int SumEvPos(int[]array)
+{
+    int sum = 0;
+    for(int i = 1; i < array.Length; i= i+2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}*/
+int SumEvPos(int[]array) // foreach обращается только к значению индекса?
+{
+    int sum = 0;
+    
+    foreach (int item in array)
+{
+    int i = 1;
+    sum+= i/2 ==0! ? item: 0;
+    
+}
+return sum;
+}
